fecdebd7e19947e3f3f621736f376e933f41f873
PTinagbedia <ptinagbedia@ubuntu.ubuntu-domain>
2012-11-01 23:36:08 +0000

    Nationawide signup WIP

--- nationwidefinance/referrals/views.py
+++ nationwidefinance/referrals/views.py
@@ -18,6 +18,46 @@ def home(request,template='index.html'):
                               dict(title='Welcome to Nationwide Finance',),
                               context_instance=RequestContext(request))
 
+
+def errorHandle(request, error):
+		form = LoginForm()
+		return render_to_response('login.html', {
+				'error' : error,
+				'form' : form,
+		}, context_instance=RequestContext(request))
+
+def nationwidelogin(request,template='login.html'):
+	#do login
+	if request.method == 'POST': # If the form has been submitted...
+		form = LoginForm(request.POST) # A form bound to the POST data
+		if form.is_valid(): # All validation rules pass
+			username = request.POST['username']
+			password = request.POST['password']
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				if user.is_active:
+					# Redirect to a success page.
+					login(request, user)				
+					return HttpResponseRedirect('/')
+
+				else:
+					# Return a 'disabled account' error message				
+					error = u'account disabled'
+					return errorHandle(request, error)
+			else:
+				 # Return an 'invalid login' error message.			
+				error = u'invalid login'
+				return errorHandle(request, error)
+		else: 
+			username = ''
+			password = ''
+			error = u'form is invalid'
+			return errorHandle(request, error)		
+	else:
+		form = LoginForm() # An unbound form
+		return render_to_response('login.html', dict(title='Nationwide Login ',), context_instance=RequestContext(request))	
+
+
 def redirect_to_home(request):
 	return HttpResponseRedirect('/')
 
@@ -110,7 +150,6 @@ def add_referral(request):
                           context_instance=RequestContext(request))
 
 
-	
 def sign_up(request,template='sign_up.html'):	
 
 	if request.method == 'GET':
