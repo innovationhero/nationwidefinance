82ee7a7161e67ebd993192ac2f52086fe3ceee10
Sam Mohamed <sam@pydevs.org>
2012-11-06 20:20:53 -0800

    updated the registration app

--- nationwidefinance/settings.py
+++ nationwidefinance/settings.py
@@ -111,7 +111,7 @@ TEMPLATE_DIRS = (
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'registration',
+    'nationwidefinance.registration',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
