3f8b1bc906b920d3d5c0435bc1225aea24694792
Sam Mohamed <sam@pydevs.org>
2012-11-09 21:03:57 -0800

    fixed indentation on mailer.py

--- nationwidefinance/mailer.py
+++ nationwidefinance/mailer.py
@@ -6,17 +6,17 @@ from django.conf import settings
 
 def send_email(subject=None, body=None, to_email=None, fail_silently=False):
 	params = dict(
-        subject = subject,
-        body = body,
-        from_email = 'admin@webiken.net',
-        to = to_email,
-    )
-    try:
-        connection = mail.get_connection(fail_silently=fail_silently)
-        connection.open()
-        email = mail.EmailMessage(**params)
-        email.content_subtype = "html"
-        email.send()
-        connection.close()
-    except ValueError, e:
-        raise ValueError('Invalid backend argument')
+		subject = subject,
+		body = body,
+		from_email = 'admin@webiken.net',
+		to = to_email,
+	)
+	try:
+		connection = mail.get_connection(fail_silently=fail_silently)
+		connection.open()
+		email = mail.EmailMessage(**params)
+		email.content_subtype = "html"
+		email.send()
+		connection.close()
+	except ValueError, e:
+		raise ValueError('Invalid backend argument')
