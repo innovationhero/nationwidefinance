ca8d61c3806ff309d6e41f9d4b5b96220a9ab706
Sam Mohamed <sam@pydevs.org>
2012-11-15 22:16:08 -0800

    url pattern redirect

--- nationwidefinance/urls.py
+++ nationwidefinance/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic.simple import redirect_to
+from django.http import HttpResponseRedirect
+
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -8,7 +10,7 @@ from django.views.generic.simple import redirect_to
 from referrals.facebook import facebook_view
 
 urlpatterns = patterns('',
-	url(r'',redirect_to, {'url': '/nationwide/referrals/'})
+	url(r'^$', lambda x: HttpResponseRedirect('/nationwide/referrals/')),
 	url(r'referrals/', include('nationwidefinance.referrals.urls')),
 	#url(r'', include('social_auth.urls')),
 	url(r'^fb/', facebook_view, name='fb_app'),
