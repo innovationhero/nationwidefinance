6ce7570b383a3b9055cb120bf2cc3959e9f2f4cf
Sam Mohamed <sam@pydevs.org>
2012-11-15 21:58:02 -0800

    unecessary sub

--- nationwidefinance/urls.py
+++ nationwidefinance/urls.py
@@ -4,10 +4,13 @@ from django.conf.urls.defaults import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-import settings
+from referrals.facebook import facebook_view
 
 urlpatterns = patterns('',
-	url(r'^%s' % (settings.SUB_SITE,), include('nationwidefinance.urls_subsite')),
+	url(r'referrals/', include('nationwidefinance.referrals.urls')),
+	url(r'', include('social_auth.urls')),
+	url(r'^fb/', facebook_view, name='fb_app'),
+	url(r'^accounts/', include('nationwidefinance.registration.urls')),
 	#url(r'^openid/', include('django_openid_auth.urls')),
 	#url(r'^accounts/profile','nationwidefinance.referrals.views.redirect_to_home'),
     
