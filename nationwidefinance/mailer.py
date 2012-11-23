921a7277cf6d2c820668f7cd87b980bff2736fb7
Sam Mohamed <sam@pydevs.org>
2012-11-23 04:02:51 -0800

    added site to new user email

--- nationwidefinance/mailer.py
+++ nationwidefinance/mailer.py
@@ -1,5 +1,6 @@
 
 from django.template.loader import get_template
+from django.contrib.sites.models import Site
 from django.template import Context
 from django.core import mail
 from django.conf import settings
@@ -25,8 +26,10 @@ def send_email(subject=None, body=None, to_email=None, fail_silently=False):
 def send_new_user_email(referrer=None, referred=None, business_name=None):
 	t = get_template('new_referrer_email.html')
 
-	c1 = Context(dict(user = referrer, business_name = business_name))
-	c2 = Context(dict(user = referred, business_name = business_name))
+	site = Site.objects.get_current()
+
+	c1 = Context(dict(user = referrer, business_name = business_name, site = site))
+	c2 = Context(dict(user = referred, business_name = business_name, site = site))
 
 	body1 = t.render(c1)
 	body2 = t.render(c2)
